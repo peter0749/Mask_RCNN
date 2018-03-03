5f57cd5073d59455529adeec6de060044bc868b1
peter0749 <jengku@gmail.com>
2018-03-03 22:24:03 +0800

    fix potential bugs

--- model.py
+++ model.py
@@ -1648,9 +1648,9 @@ class data_generator(Sequence):
             # Skip images that have no instances. This can happen in cases
             # where we train on a subset of classes and the image doesn't
             # have any of the classes we care about.
-            if not np.any(gt_class_ids > 0):
-                b -= 1 # discard this batch
-                continue # (b+=1)
+            #if not np.any(gt_class_ids > 0):
+                #continue # (b+=1)
+            # OOPS!!
 
             # RPN Targets
             rpn_match, rpn_bbox = build_rpn_targets(image.shape, self.anchors,
